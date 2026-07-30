da9e45f6b6d6cfa275f737047106e3d4d32be9fc
liliiillili <younghyun.jung.sec@gmail.com>
2026-07-30 16:40:43 +0900

    Update vm-ubuntu-desktop-terminal-tab.sh

--- vm-ubuntu-desktop-terminal-tab.sh
+++ vm-ubuntu-desktop-terminal-tab.sh
@@ -1,2 +1,5 @@
 gsettings set org.gnome.Ptyxis.Shortcuts move-next-tab '<Control>Tab'
 gsettings set org.gnome.Ptyxis.Shortcuts move-previous-tab '<Control><Shift>Tab'
+gsettings set org.gnome.desktop.wm.keybindings maximize "['<Super>Up']"
+gsettings set org.gnome.desktop.wm.keybindings unmaximize "['<Super>Down']"
+
