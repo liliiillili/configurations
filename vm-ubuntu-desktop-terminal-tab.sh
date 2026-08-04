32e412d6fb293ba45ba2965c5dddaf46d317576d
liliiillili <younghyun.jung.sec@gmail.com>
2026-08-04 09:14:29 +0900

    Update vm-ubuntu-desktop-terminal-tab.sh

--- vm-ubuntu-desktop-terminal-tab.sh
+++ vm-ubuntu-desktop-terminal-tab.sh
@@ -1,5 +1,24 @@
+# 탭
 gsettings set org.gnome.Ptyxis.Shortcuts move-next-tab '<Control>Tab'
 gsettings set org.gnome.Ptyxis.Shortcuts move-previous-tab '<Control><Shift>Tab'
+
+# 독바
+gsettings set org.gnome.shell.extensions.dash-to-dock dock-fixed false
+gsettings set org.gnome.shell.extensions.dash-to-dock autohide true
+
+# 최대화
 gsettings set org.gnome.desktop.wm.keybindings maximize "['<Super>Up']"
 gsettings set org.gnome.desktop.wm.keybindings unmaximize "['<Super>Down']"
 
+# 배경
+gsettings set org.gnome.desktop.background primary-color '#1b1b1b'
+
+# 우커맨드 이슈
+gsettings set org.gnome.desktop.input-sources xkb-options "['korean:ralt_hangul']"
+sudo setkeycodes 72 100     # 100 = KEY_RIGHTALT
+
+sudo tee /etc/udev/hwdb.d/70-hangul.hwdb << 'EOF'
+evdev:atkbd:dmi:*
+ KEYBOARD_KEY_72=rightalt
+EOF
+sudo systemd-hwdb update && sudo udevadm trigger
